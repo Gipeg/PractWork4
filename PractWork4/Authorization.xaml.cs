919f3d25fa72e188fd1f989511371a2b50a81a79
fisp <arenatkanovna@gmail.com>
2024-06-23 17:30:08 +0300

    Добавьте файлы проекта.

---
+++ PractWork4/Authorization.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PractWork4
+{
+    /// <summary>
+    /// Логика взаимодействия для Authorization.xaml
+    /// </summary>
+    public partial class Authorization : Window
+    {
+        public Authorization()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void AuthorizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (LoginTextBox.Text == Properties.Settings.Default.Login && PasswordTextBox.Text == Properties.Settings.Default.Password)
+            {
+                Settings settings = new Settings();
+                settings.Show();
+            }
+            else
+            {
+                MessageBox.Show("Неправильный логин или пароль");
+            }
+        }
+    }
+}
